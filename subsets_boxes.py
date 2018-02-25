a58545c055e58787b8fdf07c9636243e338f92bd
sbd584 <32025147+sbd584@users.noreply.github.com>
2018-02-24 21:48:23 -0600

    Update subsets_boxes.py

--- subsets_boxes.py
+++ subsets_boxes.py
@@ -4,11 +4,14 @@ def subsets (a, b, lo, d):
   if (lo == hi):
     if b != []:
       if (len(b) == 4):
-        for i in range(len(a)):
-          if (does_fit(a[i],a[i+1])):
-            print(b)
+        #for i in range(len(a)):
+        if (does_fit(b[0],b[1]) and does_fit(b[1],b[2]) and does_fit(b[2],b[3])):
+          print(b)
+          #d.append(b)
+          #print()
+          print(len(a))
         #d.append(b)
-          return
+        return
   else:
     c = b[:]
     b.append (a[lo])
@@ -18,22 +21,22 @@ def subsets (a, b, lo, d):
 def does_fit (box1, box2):
   return (box1[0] < box2[0]) and (box1[1] < box2[1]) and (box1[2] < box2[2])
 
-#def print_sets(d):
-#    print(d)
+def print_sets(d):
+    print(d)
 
 def main():
-  a = [[14, 27, 62],[16, 40, 90],[27, 50, 89],[53, 56, 91],[60, 60, 66]]
+  a = [[14, 27, 62],[16, 40, 80],[27, 50, 89],[53, 56, 91],[60, 60, 90]]
   #a = [[14, 27, 62], [15, 85, 92], [16, 40, 90], [22, 39, 56], [23, 61, 78], [23, 70, 90], [25, 93, 98], [27, 50, 89], [29, 62, 64], [32, 38, 50], [37, 43, 66], [46, 93, 95], [48, 56, 99], [49, 67, 95], [51, 52, 96], [53, 56, 91], [57, 82, 94], [60, 60, 66], [70, 74, 91], [79, 89, 91]]
   b = []
   d = []
   subsets (a, b, 0, d)
-
+  print(a[0])
 
   #for i in range len(b):
   #  for j in range len(i):
   #    for k in range (3):
   #      for m in range (2):
   #        if(does_fit(d[i[j[k]]],d[i[j[k+1]]]))
-  #print_sets(d)
+  print_sets(d)
 
 main()
