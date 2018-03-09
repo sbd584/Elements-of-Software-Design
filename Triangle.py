9c6104bb62137fa8008b3f9df3184a36061cdde0
sbd584 <32025147+sbd584@users.noreply.github.com>
2018-03-09 16:30:59 -0700

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -75,8 +75,8 @@ def dynamic_prog (grid):
   for i in range(len(grid) - 2, -1, -1):
     for j in range(i + 1):
       grid[i][j] = int(grid[i][j]) + max(int(grid[i + 1][j]), int(grid[i + 1][j + 1]))
-  print("The greatest path sum through dynamic programming is {}.".format(grid[0][0]))
-  return #grid[0][0] int(dynamic_prog(grid))
+  print("The greatest path sum through dynamic programming is ",format(grid[0][0]),".")
+  return
 
 # reads the file and returns a 2-D list that represents the triangle CHECK THIS LCODE
 def read_file (in_file):
