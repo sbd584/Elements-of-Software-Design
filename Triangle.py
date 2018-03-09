2526b80945fadf34b6dc0073bcb1704f8d6ee64d
sbd584 <32025147+sbd584@users.noreply.github.com>
2018-03-09 15:48:45 -0600

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -28,14 +28,14 @@ def exhaustive_search (grid):
   for i in range(2 ** (len(grid) - 1)):
     b = grid[0][0]
     j = 0
-    
+
     if (i == len(grid)):
       b.append(c)
   else:
     c += grid[j][i]
     if (i == 0):
       return exhaustive_search(grid, i + 1, j + 1, b, c)
-    
+
     return exhaustive_search(grid, i, j + 1, b, c)
 
 # returns the greatest path sum using greedy approach
@@ -46,14 +46,14 @@ def greedy (grid):
 
   for i in range(len(grid) - 1):
     j += grid[i][index]
-    
+
     if (grid[i + 1][index] > grid[i + 1][index + 1]):
       index = index
     else:
       index += 1
-        
+
   j += grid[i + 1][index]
-  
+
   return grid[i + 1][j]
 
 # returns the greatest path sum using divide and conquer (recursive) approach
@@ -61,12 +61,12 @@ def rec_search (grid, i, j):
 
   if (i >= len(grid)):
     return 0
-  
+
   else:
     grid1 = rec_search(grid, i + 1, j)
     grid2 = rec_search(grid, i + 1, j + 1)
     i = max(grid1, grid2) + int(grid[i][j])
-    
+
   return i
 
 # returns the greatest path sum and the new grid using dynamic programming
@@ -79,21 +79,22 @@ def dynamic_prog (grid):
   return grid[0][0]
 
 # reads the file and returns a 2-D list that represents the triangle CHECK THIS LCODE
-def read_file (file):
-      
+def read_file (in_file):
+  # create matrix
   grid = []
-
-  num_lines = file.readline()
-  num_lines = num_lines.strip()
-  num_lines = int(num_lines)
-  
+  #read number of lines
+  line = in_file.readline()
+  line = line.strip()
+  num_lines = int(line)
+  # read triangle
   for line in range(num_lines):
-    line = file.readline()
-    line = line.split()
-    for i in line:
-      line = int(i)
-      grid.append(line)
-      
+    line = in_file.readline()
+    line = line.strip()
+    row = line.split()
+    for i in range(len(row)):
+      row[i] = int(row[i])
+    grid.append(row)
+
   return grid
 
 def main ():
@@ -116,36 +117,30 @@ def main ():
   tf = time.time()
   del_t = tf - ti
   # print time taken using greedy approach
+  ##print("The greatest path sum through the greedy approach is ", int(dynamic_prog(grid)), ".")
+  ##print("The time taken for the greedy approach is", del_t, "seconds.")
 
   #The greatest path sum through greedy search is 23.
   #The time taken for greedy approach is x seconds.
 
   ti = time.time()
   # output greates path from divide-and-conquer approach
+  ##r_max = rec_search(grid, 0, 0)
+  ##r_max = max(r_max)
   tf = time.time()
   del_t = tf - ti
   # print time taken using divide-and-conquer approach
-  
-  ##r_max = rec_search(grid, 0, 0)
-  ##r_max = max(r_max)
-  ##print("The greatest path sum through dynamic programming is ", r_max, ".")
-  ##print("The time taken for dynamic programming is", del_t, "seconds.")
+  ##print("The greatest path sum through divide-and-conquer is ", r_max, ".")
+  ##print("The time taken for divide-and-conquer is", del_t, "seconds.")
 
-  #The greatest path sum through recursive search is 23.
-  #The time taken for recursive search is x seconds.
-
-  ti = time.time()
-  # output greates path from dynamic programming
+  ##ti = time.time()
+  #output greates path from dynamic programming
   dynamic_prog(grid)
   tf = time.time()
   del_t = tf - ti
   # print time taken using dynamic programming
-  
   print("The greatest path sum through dynamic programming is ", int(dynamic_prog(grid)), ".")
   print("The time taken for dynamic programming is", del_t, "seconds.")
-
-  #The greatest path sum through dynamic programming is 23.
-  #The time taken for dynamic programming is x seconds.
 
 if __name__ == "__main__":
   main()
